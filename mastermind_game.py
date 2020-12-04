6e7a2d3842c80786d270e478fa70d2e1d40ee501
Ciarra Murphy <mciarra@ymail.com>
2020-12-04 14:32:34 -0600

    added the enter handle and the starting code for the comparison

--- mastermind_game.py
+++ mastermind_game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -18,7 +19,14 @@ orange = (255, 162, 0)
 row = 0
 col = 0
 
-# creates a list of list to hold all of the initial values for the board when the code starts running; the grey pegs not be visible until the codebreaker finishes a row
+# creates a list of random numbers to use as the computer's answers for the game; these will be used to compare to the
+# user's input.
+comp_answers = [random.randint(0, 5) for x in range(4)]
+
+print(str(comp_answers))
+
+# creates a list of list to hold all of the initial values for the board when the code starts running;
+# the grey pegs not be visible until the codebreaker finishes a row
 board = [[white, white, white, white, grey, grey, grey, grey],
          [white, white, white, white, grey, grey, grey, grey],
          [white, white, white, white, grey, grey, grey, grey],
@@ -133,7 +141,8 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
                 print("Up Arrow")
-                # if the color is initially white, it assigns it to the first color in the codebreaker_colors list and updates the board
+                # if the color is initially white, it assigns it to the first color in the codebreaker_colors list
+                # and updates the board
                 if board[col][row] is codemaker_colors[0]:
                     board[col][row] = codebreaker_colors[0]
                     draw_board()
@@ -168,4 +177,32 @@ while True:
                 row += 1
                 row = row % 4
 
+            if event.key == pygame.K_RETURN:
+                print("Pressed Enter")
+
+                def evaluation_inner(guess, fakeCode):
+
+                    # Get the length n of the guess and the code.
+                    assert (len(guess) == len(fakeCode))
+                    n = len(guess)
+
+                    # Determine the correct and incorrect positions.
+                    correct_positions = [i for i in list(range(n)) if guess[i] == fakeCode[i]]
+                    incorrect_positions = [i for i in list(range(n)) if guess[i] != fakeCode[i]]
+                    num_correct = len(correct_positions)
+
+                    # Reduce the guess and the fakeCode by removing the correct positions.
+                    # Create the set values that are common between the two reduced lists.
+                    reduced_guess = [guess[i] for i in incorrect_positions]
+                    reduced_code = [fakeCode[i] for i in incorrect_positions]
+                    reduced_set = set(reduced_guess) & set(reduced_guess)
+
+                    # Determine the number of transposed values.
+                    num_transposed = 0
+                    for x in reduced_set:
+                        num_transposed += min(reduced_guess.count(x), reduced_code.count(x))
+
+                    return num_correct, num_transposed
+
     pygame.display.update()
+
